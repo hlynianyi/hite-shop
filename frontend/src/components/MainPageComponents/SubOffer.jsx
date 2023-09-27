c9cfce610fbff34098ecd198af6284eefdacd16d
vladyslav <glinyany.lie@gmail.com>
2023-09-27 23:44:53 +0200

    fix sub section responsiveness

--- frontend/src/components/MainPageComponents/SubOffer.jsx
+++ frontend/src/components/MainPageComponents/SubOffer.jsx
@@ -14,26 +14,24 @@ const SubOffer = () => {
   };
 
   return (
-    <div className="pt-4 pb-24 flex flex-col items-center bg-customgray m-0 p-0">
-      <div className="flex flex-col">
-        <p className="pt-12 pb-8 font-poiret text-center text-2xl">
-          Subscribe and get -30% offer
-        </p>
-        <div className="flex justify-between bg-customemail rounded-full">
-          <input
-            value={email}
-            onInput={handleInput}
-            className="bg-transparent outline-none w-96 px-1 text-dark text-2xl placeholder-gray pl-5 font-poiret"
-            type="email"
-            placeholder="Email"
-          />
-          <button
-            onClick={handleSubcribe}
-            className="bg-customblue hover:bg-gray-400 text-white text-2xl px-4 py-1 rounded-full font-opensans"
-          >
-            Subscribe
-          </button>
-        </div>
+    <div className="subscription bg-customgray m-0 p-0">
+      <p className="font-poiret">
+        Subscribe and get -30% offer
+      </p>
+      <div className="bg-customemail rounded-full">
+        <input
+          value={email}
+          onInput={handleInput}
+          className=" text-dark placeholder-gray font-poiret"
+          type="email"
+          placeholder="Email"
+        />
+        <button
+          onClick={handleSubcribe}
+          className="bg-customblue hover:bg-gray-400 text-white rounded-full font-opensans"
+        >
+          Subscribe
+        </button>
       </div>
     </div>
   );
